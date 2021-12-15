69878e83aba9b3efe9898fa25f3db02a58fb7e81
Leandro <leandroquiroga9514@gmail.com>
2021-12-15 17:04:51 -0300

    Has been modified the routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { pageInit, pageUs, pageTestimonials, pageTravels, pageTripDetails } from "../controller/pageControllers.js"
-
+import { pageSaveTestimonials } from "../controller/testimonialsCotroller.js"
 const router = express.Router();
 
 router.get('/', pageInit);
@@ -12,5 +12,7 @@ router.get('/viajes', pageTravels);
 router.get('/viajes/:id', pageTripDetails)
 
 router.get('/testimonios', pageTestimonials);
+
+router.post('/testimonios', pageSaveTestimonials);
 
 export default router
